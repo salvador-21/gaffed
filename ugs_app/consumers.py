279ea6ad4b7f9da72d2fa7404823f7135722e5a1
salvador-21 <salvador.cecilio721@gmail.com>
2024-05-13 01:27:50 +0800

    Channels Debug

--- ugs_app/consumers.py
+++ ugs_app/consumers.py
@@ -3,6 +3,8 @@ from random import randint
 from time import sleep
 from channels.generic.websocket import WebsocketConsumer,AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
+from .models import UserAccount,UserWallet,Games
+
 
 
 class WSConsumer(AsyncWebsocketConsumer):
@@ -15,11 +17,14 @@ class WSConsumer(AsyncWebsocketConsumer):
 
     async def receive(self,text_data):
           text_data_json=json.loads(text_data)
-          
+          games = await get_games()
           print(text_data_json)
 
           await self.send(text_data=json.dumps({
-                'message': 'ggisreal',
+                'message': games,
           }))
     
 
+@database_sync_to_async
+def get_games():
+ return Games.objects.all()[0].g_name
